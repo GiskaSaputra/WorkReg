55d6f78c8ad7165ddcb4050f0a216117991f0d5d
Giska Saputra <giskasaputra2005@gmail.com>
2026-04-20 11:58:14 +0700

    Koneksi

--- konfigurasi/Koneksi.cs
+++ konfigurasi/Koneksi.cs
@@ -1,12 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
-namespace WorkReg.konfigurasi
+namespace SIRaM.konfigurasi
 {
-    internal class koneksi
+    internal class Koneksi : Konfigurasi
     {
+        MySqlConnection conn;
+        MySqlCommand cmd;
+        MySqlDataAdapter adapter;
+        string Link = "server=localhost;port=3306;database=db_siram;uid=root;pwd=";
+        public Koneksi()
+        {
+            conn = new MySqlConnection(Link);
+            cmd = new MySqlCommand();
+            adapter = new MySqlDataAdapter();
+        }
+
+        private void bukaKoneksi()
+        {
+            try
+            {
+                if(conn.State==ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+        public void tutupKoneksi()
+        {
+            conn.Close();
+        }
+
+        public override int eksekusiBukanQuery(string query)
+        {
+            int nilai = -1;
+
+            try
+            {
+                bukaKoneksi();
+                cmd.Connection=conn;
+                cmd .CommandText=query;
+                nilai=cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex) { }
+            finally { tutupKoneksi(); }
+
+            return nilai;
+        }
+
+        public override DataTable eksekusiQuery(string query)
+        {
+            DataTable nilai = new DataTable();
+
+            try
+            {
+                bukaKoneksi();
+                cmd.Connection = conn;
+                cmd.CommandText = query;
+                adapter.SelectCommand = cmd;
+                adapter.Fill(nilai);
+            }
+            catch (Exception ex) { }
+            finally { tutupKoneksi(); }
+
+            return nilai;
+        }
     }
 }
--- konfigurasi/Konfigurasi.cs
+++ konfigurasi/Konfigurasi.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
-namespace WorkReg.konfigurasi
+namespace SIRaM.konfigurasi
 {
-    internal class Konfigurasi
+    abstract class Konfigurasi
     {
+        //untuk menangani instruksi INSERT, UPDATE ,dan DELETE
+        //function
+        public abstract int eksekusiBukanQuery(string query);
+
+        //untuk menangani proses SELECT
+        public abstract DataTable eksekusiQuery(string query);
     }
 }
